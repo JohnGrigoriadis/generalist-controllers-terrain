23a5cbf6d358d343123333a8148eb3f3d6933f7b
John Grigoriadis <113774274+JohnGrigoriadis@users.noreply.github.com>
2024-01-03 20:22:17 +0200

    Update DDPG_Agent.py
    made the agent train with the total episode instead of each state individually. The agent still stops moving after a couple seconds.
    
    This might mean that the movements get smaller and smaller as time goes on.

--- DDPG_Agent.py
+++ DDPG_Agent.py
@@ -10,68 +10,31 @@ import keyboard as kb
 
 env = gym.make('BipedalWalker-v3', max_episode_steps=1600)
 
-# Simpler Neural Networks. Simpler NN will make the code faster but probably less effective
-# class Actor(nn.Module):
-#     def __init__(self, state_size, action_size):
-#         super(Actor, self).__init__()
-#         self.fc1 = nn.Linear(state_size, 64)
-#         self.fc2 = nn.ReLU()
-#         self.fc3 = nn.Linear(64, action_size)
-#         self.tanh = nn.Tanh()
-
-#     def forward(self, state):
-#         x = torch.relu(self.fc1(state))
-#         x = torch.relu(self.fc2(x))
-#         x = self.tanh(self.fc3(x))
-#         return x
 class Actor(nn.Module):
     def __init__(self, state_size, action_size):
         super(Actor, self).__init__()
-        self.fc1 = nn.Linear(state_size, 128)
-        self.fc2 = nn.ReLU()
-        self.fc3 = nn.Linear(128, 64)
-        self.fc4 = nn.ReLU()
-        self.fc5 = nn.Linear(64, action_size)
-        self.tanh = nn.Tanh()
+        self.fc1 = nn.Linear(state_size, 64)
+        self.fc2 = nn.Linear(64, action_size)
+        self.relu = nn.ReLU()
 
     def forward(self, state):
-        x = torch.relu(self.fc1(state))
-        x = torch.relu(self.fc2(x))
-        x = torch.relu(self.fc3(x))
-        x = torch.relu(self.fc4(x))
-        x = self.tanh(self.fc5(x))
+        x = self.relu(self.fc1(state))
+        x = self.fc2(x)
         return x
 
-# FILEPATH: /d:/University/Thesis/Generalist Controllers/Getting somewhere/DDPG_Agent.py
+
 class Critic(nn.Module):
     def __init__(self, state_size, action_size):
         super(Critic, self).__init__()
-        self.fc1 = nn.Linear(state_size + action_size, 128)
+        self.fc1 = nn.Linear(state_size + action_size, 64)
         self.fc2 = nn.ReLU()
-        self.fc3 = nn.Linear(128, 64)
-        self.fc4 = nn.ReLU()
-        self.fc5 = nn.Linear(64, 1)
+        self.fc3 = nn.Linear(64, 1)
 
     def forward(self, state, action):
         x = torch.relu(self.fc1(torch.cat([state, action], 1)))
         x = torch.relu(self.fc2(x))
-        x = torch.relu(self.fc3(x))
-        x = torch.relu(self.fc4(x))
-        x = self.fc5(x)
+        x = self.fc3(x)
         return x
-
-# class Critic(nn.Module):
-#     def __init__(self, state_size, action_size):
-#         super(Critic, self).__init__()
-#         self.fc1 = nn.Linear(state_size + action_size, 64)
-#         self.fc2 = nn.ReLU()
-#         self.fc3 = nn.Linear(64, 4)
-
-#     def forward(self, state, action):
-#         x = torch.relu(self.fc1(torch.cat([state, action], 1)))
-#         x = torch.relu(self.fc2(x))
-#         x = self.fc3(x)
-#         return x
 
 # Define the DDPG Agent class
 class DDPGAgent:
@@ -86,11 +49,14 @@ class DDPGAgent:
         self.critic = Critic(state_size, action_size).to(self.device)
         self.target_actor = Actor(state_size, action_size).to(self.device)
         self.target_critic = Critic(state_size, action_size).to(self.device)
-        self.actor_optimizer = optim.Adam(self.actor.parameters(), lr=0.001)
-        self.critic_optimizer = optim.Adam(self.critic.parameters(), lr=0.002)
-        self.update_target_models(tau=0.1)
+
+        self.actor_optimizer = optim.Adam(self.actor.parameters(), lr=0.0001)
+        self.critic_optimizer = optim.Adam(self.critic.parameters(), lr=0.0002)
+
+        self.update_target_models(tau=0.01)
 
         self.trained = False
+        self.action_size = action_size
 
         # When reusing the model, the model can be loaded here.
         if load_path is not None:
@@ -117,6 +83,20 @@ class DDPGAgent:
             action = self.actor(state).cpu().numpy()
         return action
 
+    # def get_action(self, state, noise_std=0.7):
+    #     state = torch.FloatTensor(state).to(self.device)
+    #     with torch.no_grad():
+    #         action = self.actor(state).cpu().numpy()
+    #         action += noise_std * np.random.randn(self.action_size)
+    #     return action
+    
+    # def get_action(self, state, noise_std=0.1):
+    #     state = torch.FloatTensor(state).to(self.device)
+    #     with torch.no_grad():
+    #         action = self.actor(state).cpu().numpy()
+    #         action += noise_std * np.random.randn(self.action_size)
+    #     return action
+
     def train(self, states, actions, rewards, next_states, dones, gamma=0.99):
         
         # Prints a message when the agent starts training
@@ -132,9 +112,9 @@ class DDPGAgent:
 
         states = torch.FloatTensor(states).to(self.device)
         actions = torch.FloatTensor(actions).to(self.device)
-        rewards = torch.FloatTensor(rewards).to(self.device)
+        rewards = torch.FloatTensor(rewards).unsqueeze(1).to(self.device)
         next_states = torch.FloatTensor(next_states).to(self.device)
-        dones = torch.FloatTensor(dones).to(self.device)
+        dones = torch.FloatTensor(dones).unsqueeze(1).to(self.device)
 
         target_actions = self.target_actor(next_states)
         target_q_values = self.target_critic(next_states, target_actions)
@@ -150,7 +130,8 @@ class DDPGAgent:
         critic_loss.backward()
         self.critic_optimizer.step()
 
-        self.update_target_models(tau=0.1)
+        self.update_target_models(tau=0.01)
+
 
 # Create the DDPG agent
 state_size = env.observation_space.shape[0]
@@ -163,45 +144,61 @@ def save_agent(model_name):
         'critic_state_dict': agent.critic.state_dict(),
         'actor_optimizer_state_dict': agent.actor_optimizer.state_dict(),
         'critic_optimizer_state_dict': agent.critic_optimizer.state_dict()}, 
-            f'Saved models/{model_name}.pth')
-    print("Model saved")
+            f'Saved models/{model_name}')
+    print(f"Model Saved in Saved models/{model_name}")
     
 if __name__ == "__main__":
-    print("This is a module, not a script. Please import this module to use it.")
+    print("The file is run directly")
 
     # Set the model name so it can be saved
-    model_name = "ddpg_agent_test.pth"
+    model_name = "ddpg_agent_test_3.pth"
 
+    # action_list = []
 
     # Training the agent
-    episodes = 1000
+    episodes = 100
     for epoch in range(episodes + 1):
         state, _ = env.reset()
         total_reward = 0
 
+        # Lists to store trajectory information
+        states, actions, rewards, next_states, dones = [], [], [], [], []
+
         while True:
             action = agent.get_action(state)
+            # action_list.append(action)
             next_state, reward, done, truncated, _ = env.step(action)
-            
-            agent.train([state], [action], [reward], [next_state], [done])
+
+            # Collect trajectory information
+            states.append(state)
+            actions.append(action)
+            rewards.append(reward)
+            next_states.append(next_state)
+            dones.append(done)
 
             state = next_state
             total_reward += reward
 
             if done or truncated:
+                # Train the agent at the end of the episode
+                agent.train(states, actions, rewards, next_states, dones)
+
                 # Only print every n-th episode. Depends on the number of episodes
-                if (epoch+1) % 1 == 0:
+                if (epoch + 1) % 10 == 0:
                     print(f"Episode: {epoch + 1}, Total Reward: {total_reward}")
                 break
-            
+
             # In case the training shows no progress, the training can be stopped manually
             if kb.is_pressed("q"):
                 env.close()
+                print("Manual break")
                 save_agent(model_name)
-                # print("Training manually stopped - Model saved")
                 exit()
 
-    env.close()
+        env.close()
 
     # Save the trained DDPG agent
     save_agent(model_name)
+
+    # print(actions)
+    
