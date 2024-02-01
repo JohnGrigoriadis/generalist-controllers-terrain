42f2eddcbf950c3cf456821d9c19b1cd4b8bb662
John Grigoriadis <113774274+JohnGrigoriadis@users.noreply.github.com>
2024-02-01 14:42:36 +0100

    Working code for evolution, mutation and evaluation.
    This is the working version of my XNES replica for doing the mutation, evaluation and evolution of the weights of the NN while changing the terrains.

--- evolution.py
+++ evolution.py
@@ -1,4 +1,4 @@
-# import gymnasium as gym
+
 import numpy as np
 from typing import List
 
@@ -6,9 +6,23 @@ import torch
 import torch.nn as nn
 # import torch.nn.functional as F
 
-# import keyboard
+import keyboard as kb # Used to stop the simulation manually if needed
+import time # Used to time the simulation only during testing
 
 from biped_terrain import BipedalWalker 
+
+
+"""
+Things to do:
+1. Find why the simulations don't stop running
+    a. prob because i wasn't incrementing s # fixed
+2. See if the noise added to the weights is correct (maybe ther eis a better way to do it)
+3. The first simulation seems to be in terrain 2, not terrain 1. Why? #fixed
+
+Notes to self:
+1. O(g,p) worst case time complexity is about 25 seconds for g=5 and p=10
+
+"""
 
 noise_range = [0.0, 1.1] # I want to include 1.0 
 slope_range = [0.0, 1.1] # I want to include 1.0
@@ -22,28 +36,30 @@ def generate_terrain(noise_range, slope_range, step_size):
 
     return terrains
 
-# Anil did all this manually, but I think a normal NN structure would work fine.
+# A normal NN structure would work fine.
 
 class NeuralNetwork(nn.Module):
     def __init__(self, state_size, action_size):
         super(NeuralNetwork, self).__init__()  # Call the parent class constructor
         self.layer1 = nn.Linear(state_size, 20)  
-        self.layer2 = nn.Linear(20, action_size)  
-        self.tanh = nn.Tanh()
-
+        self.layer2 = nn.Linear(20, action_size)
+        self.tahn = nn.Tanh()
+    
     def forward(self, state):
-        state = torch.Tensor(state) # Convert state to a Tensor and 
-        # print(state)
-        x = self.tanh(self.layer1(state))
-        x = self.tanh(self.layer2(x))
+        state = torch.Tensor(state) # Convert state to a Tensor
+        # state = state.view(1, -1)  # Reshape the state tensor to be a matrix
+        x = self.tahn(self.layer1(state))
+        x = self.tahn(self.layer2(x))
         return x
 
     def get_weights(self):
         return [self.layer1.weight, self.layer2.weight]
     
     def set_weights(self, weights):
-        self.layer1.weight = nn.Parameter(weights[0])
-        self.layer2.weight = nn.Parameter(weights[1])
+        with torch.no_grad():
+            self.layer1.weight = nn.Parameter(weights[0])
+            self.layer2.weight = nn.Parameter(weights[1])
+            # print(self.layer1.weight)
     
 
 # Test to make sure that the NN and weights are working
@@ -52,7 +68,7 @@ class NeuralNetwork(nn.Module):
 # weights
 
 
-class XNES_Replica():
+class EVO():
     """
     This class creates mutations of the weights of the neural network and then tests them on one tenvironment.
     Then selects the best performing mutation and returns the weights of that mutation on the next environment.
@@ -69,6 +85,7 @@ class XNES_Replica():
     """
     
     def __init__(self, pop_num : int, env : BipedalWalker, network : NeuralNetwork, terrains: List[List[float]], generations : int=1008) -> None:
+        
         self.pop_num = pop_num
         self.env = env
         self.network = network
@@ -77,10 +94,16 @@ class XNES_Replica():
 
         self.population = []
         self.ter_num = 0
-        self.best_reward_per_gen = None
+        self.best_per_gen = []
+        self.steps = 1600 # The number of steps to run the simulation for
+
+        # Using GPU will speed up the process. So I hope it works.
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        if torch.cuda.is_available():
+            print("Using GPU")
 
 
-    def mutate(self, weights: List[List[torch.Tensor]]) -> None:
+    def mutate(self, weights: List[torch.Tensor]) -> None:
         """
         This function takes in the weights of the network and creates a list with the new mutated population.
         """
@@ -91,7 +114,7 @@ class XNES_Replica():
             new_pop.append([weight + torch.randn_like(weight) for weight in weights])
 
         self.population = new_pop
-
+       
 
     def run_simulation(self, weights: List[List[torch.Tensor]]) -> float:
         """
@@ -99,19 +122,25 @@ class XNES_Replica():
         """
 
         self.env.noise, self.env.slope = self.terrains[self.ter_num] 
-        state, _ = self.env.reset() 
+        state, _ = self.env.reset()
         self.network.set_weights(weights)
         total_reward = 0
+        s = 0
         while True:
-            action = self.network(state).cpu().numpy()
+            state = torch.FloatTensor(state).to(self.device)
+            with torch.no_grad():
+                action = self.network(state).detach().numpy()
             state, reward, done, truncated, _ = self.env.step(action)
+
             total_reward += reward
             
-            if done or truncated:
+            s += 1
+            if done or truncated or (kb.is_pressed('q') and kb.is_pressed("l")) or s > self.steps:
                 break
         
         
         return total_reward
+    
 
     def run_and_return_best(self, population) -> List[torch.Tensor]:
         """
@@ -122,18 +151,17 @@ class XNES_Replica():
         for individual in population:
             rewards.append(self.run_simulation(individual))
         
-        self.ter_num += 1
-        if self.ter_num == len(self.terrains):
-            self.ter_num = 0
-        
-        self.best_reward_per_gen = max(rewards)
+        self.best_per_gen.append([population[np.argmax(rewards)], max(rewards)])
+
         return population[np.argmax(rewards)]
     
+
     def main(self) -> None:
         """
         This function runs the whole algorithm for the given number of generations..
         
         Process:
+
         retrieve initial weights
         start loop
 
@@ -144,12 +172,13 @@ class XNES_Replica():
         get best performing weights (run_and_return_best) on terrain 2
 
         ...
+
         """
 
         # Get initial weights
         weights = self.network.get_weights()
-        weights = self.run_and_return_best(weights)
-
+        weights = self.run_and_return_best([weights])
+ 
         # Start loop
         for gen in range(self.generations - 1):
             # Mutate weights
@@ -158,28 +187,40 @@ class XNES_Replica():
             # Get best performing weights
             weights = self.run_and_return_best(self.population)
 
-            if gen % 100 == 0:
-                print(f"Generation: {gen}")
-                print(f"Best reward: {self.best_reward_per_gen}")
+            self.ter_num += 1
+            if self.ter_num == len(self.terrains):
+                self.ter_num = 0
+
+
+            if gen % 1 == 0:
+                print(f"Generation: {gen+1}")
+                print(f"Best reward: {self.best_per_gen[-1][-1]}")
+                print(f"Terrain: {self.ter_num}")
                 print("")
 
-        return weights
+        return self.best_per_gen
+
+    
+if __name__ == "__main__":
+    # start = time.time()
+    env = BipedalWalker()
+
+    state_size, action_size = 24, 4
+    network = NeuralNetwork(state_size, action_size)
 
 
-env = BipedalWalker()
+    terrain_params = generate_terrain(noise_range, slope_range, step_size) # 36 terrains at the moment with the current step size
+    # print(terrain_params.shape) 
 
-state_size, action_size = 24, 4
-network = NeuralNetwork(state_size, action_size)
+    population_size = 10
+    generations = 5
+    xnes = EVO(population_size, env, network, terrain_params, generations)
 
-
-terrain_params = generate_terrain(noise_range, slope_range, step_size) # 36 terrains at the moment with the current step size
-# print(terrain_params.shape) 
-
-population_size = 10
-generations = 5
-xnes = XNES_Replica(population_size, env, network, terrain_params, generations)
-
-# Lets hope this works
-generalist_weights = xnes.main()
-
-torch.save(generalist_weights, 'best_weights.pt')
+    # Lets hope this works
+    generalist_weights = xnes.main()
+    
+    # end = time.time()
+    # print(f"Time taken: {end - start}")
+    
+    # print(generalist_weights[-1])
+    torch.save(generalist_weights, 'generalist-controllers-terrain/Terrain Gen/best_weights.pt')
